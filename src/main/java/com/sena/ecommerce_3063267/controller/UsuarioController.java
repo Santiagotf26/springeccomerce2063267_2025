d39b493f5d45e986629c7425c633080e829f2a7f
Santiago_tf26 <santiagotrujilloes82@gmail.com>
2025-08-29 15:58:39 -0500

    agregue documentos_truji

--- src/main/java/com/sena/ecommerce_3063267/controller/UsuarioController.java
+++ src/main/java/com/sena/ecommerce_3063267/controller/UsuarioController.java
@@ -1,15 +1,24 @@
 package com.sena.ecommerce_3063267.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sena.ecommerce_3063267.service.IProductoService;
 
 @Controller
 @RequestMapping("/")
 public class UsuarioController {
 	
+	// instancia de producto service
+	@Autowired
+	private IProductoService productoService;
+	
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("producto", productoService.findAll());
 		return"usuario/home";
 	}
 
